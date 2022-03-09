7b4112e47139e7385023cf53d76aa7b8a9641af6
Ishu Gupta <60650077+ishu9565@users.noreply.github.com>
2022-03-09 10:42:05 +0530

    Time: 36 ms (6.32%), Space: 31.8 MB (5.04%) - LeetHub

--- 22-generate-parentheses/22-generate-parentheses.cpp
+++ 22-generate-parentheses/22-generate-parentheses.cpp
@@ -1,20 +1,20 @@
 class Solution {
 public:
-    vector<string > ans;
-       void check(string s, int n, int score){
+      vector<string> generateParenthesis(int n) {
+        vector<string > ans;
+        string s="";
+        check(ans,s,n,0);
+        return ans;
+    }
+       void check(vector<string> &ans,string s, int n, int score){
         if(score<0) return;
         if(s.size()>2*n) return;
         if(score==0&&s.length()==2*n) ans.push_back(s);
-        check(s+"(",n,score+1);
-        check(s+")",n,score-1);
+        check(ans,s+"(",n,score+1);
+        check(ans,s+")",n,score-1);
            return;
     }
-    vector<string> generateParenthesis(int n) {
-        
-        string s="";
-        check(s,n,0);
-        return ans;
-    }
+  
     
  
     
