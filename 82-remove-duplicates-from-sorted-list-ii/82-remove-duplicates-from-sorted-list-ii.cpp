8df78b38b17311baab06c3decc1b5a3e00537fb5
Ishu Gupta <60650077+ishu9565@users.noreply.github.com>
2022-03-09 12:33:42 +0530

    Time: 11 ms (54.66%), Space: 12 MB (6.73%) - LeetHub

--- 82-remove-duplicates-from-sorted-list-ii/82-remove-duplicates-from-sorted-list-ii.cpp
+++ 82-remove-duplicates-from-sorted-list-ii/82-remove-duplicates-from-sorted-list-ii.cpp
@@ -10,28 +10,41 @@ public:
             m[temp->val]++;
             temp=temp->next;
         }  
-         ListNode* newhead = new ListNode(-1);
-        ListNode* prev = newhead;
-        newhead->next = head;
+//          ListNode* newhead = new ListNode(-1);
+//         ListNode* prev = newhead;
+//         newhead->next = head;
         
-        while(head && head->next)
-        {
-            if(head->val==head->next->val)
-            {
-                while(head->next && head->val==head->next->val)
-                {
-                    head=head->next;
-                }
-                head=head->next;
-                prev->next = head;
-            }
-            else
-            {
-                prev=head;
-                head=head->next;
+//         while(head && head->next)
+//         {
+//             if(head->val==head->next->val)
+//             {
+//                 while(head->next && head->val==head->next->val)
+//                 {
+//                     head=head->next;
+//                 }
+//                 head=head->next;
+//                 prev->next = head;
+//             }
+//             else
+//             {
+//                 prev=head;
+//                 head=head->next;
                     
+//             }
+//         }
+//         return newhead->next;
+        
+        
+        ListNode* newnode= new ListNode(-1);
+        ListNode* ans=newnode;
+        while(head){
+            if(m[head->val]==1){
+                ListNode* temp= new ListNode(head->val);
+                newnode->next=temp;
+                newnode=newnode->next;
             }
+            head=head->next;
         }
-        return newhead->next;
+        return ans->next;
     }
 };
