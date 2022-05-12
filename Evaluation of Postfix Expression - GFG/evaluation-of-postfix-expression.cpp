3cce7a238d7b669966ba6d3e50bc2369ded7641b
Ishu Gupta <60650077+ishu9565@users.noreply.github.com>
2022-05-12 12:43:15 +0530

    Added solution - LeetHub

--- Evaluation of Postfix Expression - GFG/evaluation-of-postfix-expression.cpp
+++ Evaluation of Postfix Expression - GFG/evaluation-of-postfix-expression.cpp
@@ -14,29 +14,26 @@ class Solution
     {
         stack<int> st;
         for(int i=0;i<S.size();i++){
-            
             if(S[i]=='+'||S[i]=='-'||S[i]=='*'||S[i]=='/')
             st.push(S[i]);
             else
             st.push(S[i]-'0');
-           // cout<<st.top()<<endl;
-            if(st.top()==43||st.top()==42||st.top()==47||st.top()==45)
+            if(st.top()=='+'||st.top()=='*'||st.top()=='/'||st.top()=='-')
             {
-               // cout<<"abc"<<endl;
-                int ch=st.top();
+                char ch=st.top();
                 st.pop();
                 int t1=st.top();
                 st.pop();
                 int t2=st.top();
                 st.pop();
                 int t3;
-                if(ch==43)
+                if(ch=='+')
                  t3= t1+t2; 
-                else  if(ch==45)
+                else  if(ch=='-')
                  t3= t2-t1; 
-                else  if(ch==42)
+                else  if(ch=='*')
                  t3= t1*t2; 
-                else  if(ch==47)
+                else  if(ch=='/')
                  t3= t2/t1; 
                 st.push(t3);
                // cout<<st.top()<<endl;
