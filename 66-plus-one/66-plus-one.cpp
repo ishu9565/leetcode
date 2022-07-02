53f037c473baa96f99d48e85abffa5bed32041e6
Ishu Gupta <60650077+ishu9565@users.noreply.github.com>
2022-07-02 20:40:36 +0530

    Time: 0 ms (100.00%), Space: 8.9 MB (12.01%) - LeetHub

---
+++ 66-plus-one/66-plus-one.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& d) {
+        int n=d.size();
+        for(int i=n-1;i>=0;i--){
+            // cout<<"D";
+            if(d[i]!=9){ d[i]=d[i]+1; return d;}
+            else{ d[i]=0;}
+           
+        }
+        d.push_back(1);
+        reverse(d.begin(),d.end());
+        return d;
+    }
+};
