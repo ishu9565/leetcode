d376ad48936b944f0f111cb925a5cdab374957b8
Ishu Gupta <60650077+ishu9565@users.noreply.github.com>
2022-07-04 00:55:34 +0530

    Time: 10 ms (64.26%), Space: 6.1 MB (48.58%) - LeetHub

---
+++ 12-integer-to-roman/12-integer-to-roman.cpp
@@ -0,0 +1,62 @@
+class Solution {
+public:
+    string intToRoman(int num) {
+        string ans="";
+        while(num>0){
+        if(num>=1000){
+            num=num-1000;
+            ans+="M";
+        }
+        if(num>=900&&num<1000){
+            num=num-900;
+            ans+="CM";
+        }
+        if(num>=500&&num<1000){
+            num=num-500;
+            ans+="D";
+        }
+        if(num>=400&&num<500){
+            num=num-400;
+            ans+="CD";
+        }
+        if(num>=100&&num<500){
+            num=num-100;
+            ans+="C";
+        }
+        if(num>=90&&num<100){
+            num=num-90;
+            ans+="XC";
+        }
+        if(num>=50&&num<100){
+            num=num-50;
+            ans+="L";
+        }
+        if(num>=40&&num<50){
+            num=num-40;
+            ans+="XL";
+        }
+        if(num>=10&&num<50){
+            num=num-10;
+            ans+="X";
+        }
+        if(num==9){
+            num=num-9;
+            ans+="IX";
+        }
+        if(num!=9&&num!=4&&num<10&&num>=5){
+            num=num-5;
+            ans+="V";
+        }
+        if(num==4){
+            num=num-4;
+            ans+="IV";
+        }
+      
+        if(num<4&&num>0){
+            num=num-1;
+            ans+="I";
+        }
+        }
+       return ans; 
+    }
+};
