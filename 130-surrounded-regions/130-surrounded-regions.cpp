b912c25b5570a7f982fc35aaf76d701096fb680e
Ishu Gupta <60650077+ishu9565@users.noreply.github.com>
2022-07-03 22:48:29 +0530

    Time: 24 ms (35.28%), Space: 10 MB (77.49%) - LeetHub

---
+++ 130-surrounded-regions/130-surrounded-regions.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    void dfs(vector<vector<char>>& board,int i,int j){
+        if(i<0||j<0||i>(board.size()-1)||j>(board[0].size()-1)||(board[i][j]!='O')) return ;
+        board[i][j]='A';
+        cout<<i<<" "<<j<<endl;
+         if(i>0) dfs(board,i-1,j);
+         if(j>0) dfs(board,i,j-1);
+         if(i<board.size()-1) dfs(board,i+1,j);
+         if(j<board[0].size()-1) dfs(board,i,j+1);
+    } 
+        
+    void solve(vector<vector<char>>& board) {
+        int m=board.size(),n=board[0].size();
+        for(int i=0;i<n;i++){
+            if(board[0][i]=='O'){
+                dfs(board,0,i);
+            }
+            if(board[m-1][i]=='O'){
+                dfs(board,m-1,i);
+            }
+        }
+        for(int i=0;i<m;i++){
+            if(board[i][0]=='O'){
+                dfs(board,i,0);
+            }
+            if(board[i][n-1]=='O'){
+                dfs(board,i,n-1);
+            }
+        }
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(board[i][j]!='A') board[i][j]='X';
+                else board[i][j]='O';
+            }
+        }
+    }
+};
