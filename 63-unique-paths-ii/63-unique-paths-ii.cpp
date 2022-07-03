905d0fccedab66e849cd14b1c1cde8d0e3e7ab9f
Ishu Gupta <60650077+ishu9565@users.noreply.github.com>
2022-07-03 16:13:03 +0530

    Time: 30 ms (5.15%), Space: 7.5 MB (90.44%) - LeetHub

---
+++ 63-unique-paths-ii/63-unique-paths-ii.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+      int dp[100][100];    
+    int unique(vector<vector<int>>& o,int x,int y) {
+        
+        if(x<0||y<0) return 0;
+        if(x==0&&y==0&&o[x][y]!=1) return 1;
+        if(o[x][y]==1) return 0;
+        if(dp[x][y]!=0) return dp[x][y];
+        int left=unique(o,x-1,y);
+        int right=unique(o,x,y-1);
+        return dp[x][y]=left +right;
+    }
+    
+    int uniquePathsWithObstacles(vector<vector<int>>& o) {
+        int m=o.size()-1,n=o[0].size()-1;
+      
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                dp[i][j]=0;
+            }
+        }
+      return  unique(o,o.size()-1,o[0].size()-1);
+        
+    }
+};
