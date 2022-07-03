79aa165de5f74949024c5bb66f74d1b02320cef2
Ishu Gupta <60650077+ishu9565@users.noreply.github.com>
2022-07-03 16:47:55 +0530

    Time: 0 ms (100.00%), Space: 6.6 MB (25.56%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -1,33 +1,17 @@
 class Solution {
 public:
-         int dp[100][100];    
-    int unique(vector<vector<int>>& o,int x,int y) {
-        
-        if(x<0||y<0) return 0;
-        if(x==0&&y==0&&o[x][y]!=1) return 1;
-        if(o[x][y]==1) return 0;
-        if(dp[x][y]!=0) return dp[x][y];
-        int left=unique(o,x-1,y);
-        int right=unique(o,x,y-1);
-        return dp[x][y]=left +right;
-    }
     
-    int uniquePathsWithObstacles(vector<vector<int>>& o) {
-        int m=o.size()-1,n=o[0].size()-1;
-      
-      return  unique(o,o.size()-1,o[0].size()-1);
-        
+    int unique(int m, int n, vector<vector<int>>&dp){
+        if(n<0||m<0) return 0;
+        if(n==0||m==0) return 1;   
+        if(dp[m][n]!=0) return dp[m][n];
+         int left=unique(m-1,n,dp);
+        int right=unique(m,n-1,dp);
+        return dp[m][n]=left +right;
     }
     
     int uniquePaths(int m, int n) {
-        vector<vector<int>>o(m,vector<int>(n));
-         for(int i = 0; i < m; i++)
-    {
-        for(int j = 0; j < n; j++)
-        {
-            o[i][j]=0;
-        }
-    }
-       return  uniquePathsWithObstacles(o);
+         vector<vector<int>> dp(m,vector<int>(n,0));
+        return unique(m-1,n-1, dp);
     }
 };
