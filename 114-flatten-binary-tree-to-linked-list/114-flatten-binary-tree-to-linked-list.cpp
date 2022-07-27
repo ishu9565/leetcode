2378847824eb520bd1d7fe4760543dba29deb8f9
Ishu Gupta <60650077+ishu9565@users.noreply.github.com>
2022-07-27 23:15:32 +0530

    Time: 13 ms (33.77%), Space: 13.2 MB (6.68%) - LeetHub

---
+++ 114-flatten-binary-tree-to-linked-list/114-flatten-binary-tree-to-linked-list.cpp
@@ -0,0 +1,56 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<TreeNode*> v;
+    void printPreorder(TreeNode* node) 
+{ 
+    if (node == NULL) 
+        return; 
+  
+    /* first print data of node */
+    v.push_back(node);
+  
+    /* then recur on left subtree */
+    printPreorder(node->left); 
+  
+    /* now recur on right subtree */
+    printPreorder(node->right); 
+}
+    
+    void flatten(TreeNode* root) {
+        if(!root) return ;
+        v.clear();printPreorder(root);
+        root->left=NULL;root->right=NULL;
+        TreeNode* p=root;
+        for(int i=1;i<v.size();i++)
+        { 
+            p->right=v[i];
+            p->left=NULL; p=p->right; p->left=NULL;p->right=NULL;
+        }
+        /*
+        TreeNode* ans=new TreeNode(v[0]);
+      
+        TreeNode* temp=NULL;
+        temp=ans;
+        int t=1;
+        while(t<v.size()){
+           TreeNode* te=new TreeNode(v[t]);
+            temp->right=te;
+            temp->left=NULL;
+            temp=temp->right;
+            t++;
+        }
+        root=ans;*/
+        
+    }
+};
