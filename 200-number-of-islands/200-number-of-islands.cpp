dcfee2367978f7a52a363249979cfec4decd70cf
Ishu Gupta <60650077+ishu9565@users.noreply.github.com>
2022-08-17 20:25:08 +0530

    Time: 48 ms (66.64%), Space: 19.1 MB (9.54%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.cpp
+++ 200-number-of-islands/200-number-of-islands.cpp
@@ -1,30 +1,45 @@
 class Solution {
-    private :
- void erase_island(vector<vector<char>>& g,int i, int j){
-       int r=g.size(),c=g[0].size();
-       if(i<0||i==r||j<0||j==c|| g[i][j]=='0'){
-           return ;
-       }
-       g[i][j]='0';
-       erase_island(g,i-1,j);
-       erase_island(g,i+1,j);
-       erase_island(g,i,j-1);
-       erase_island(g,i,j+1);
-   };
- 
 public:
-     
-    int numIslands(vector<vector<char>>& g) {
-        
-        int r=g.size(),c=g[0].size(),cnt=0;
-        for(int i=0;i<r;i++){
-            for(int j=0;j<c;j++){
-                if(g[i][j]=='1'){
-                    cnt++;
-                    erase_island(g,i,j);
+    
+    void bfs(vector<vector<char>>& grid,vector<vector<int>>& vis,int i, int j){
+         int r=grid.size();
+        int c=grid[0].size();
+        vis[i][j]=1;
+        queue<pair<int,int>>q;
+        q.push({i,j});
+        int q1[]={-1,0,1,0,-1};
+        while(!q.empty()){
+            int a1=q.front().first;
+            int a2=q.front().second;
+            q.pop();
+            // cout<<a1<<" "<<a2<<"FFFFFFF"<<endl;
+            for(int t=0;t<4;t++){
+                int n1=a1+q1[t];
+                int n2=a2+q1[t+1];
+                // cout<<n1<<" "<<n2<<endl;
+                if(n1>=0&&n2>=0&&n1<r&&n2<c&&vis[n1][n2]==0&&grid[n1][n2]=='1'){
+                    vis[n1][n2]=1;
+                    // cout<<n1<<" "<<n2<<endl;
+                    q.push({n1,n2});
                 }
             }
         }
-        return cnt;        
+    }
+    int numIslands(vector<vector<char>>& grid) {
+        int r=grid.size();
+        int c=grid[0].size();
+        
+        vector<vector<int>> vis(r,vector<int> (c,0));
+        int cnt=0;
+        for(int i=0;i<r;i++){
+            for(int j=0;j<c;j++){
+                if(vis[i][j]==0&&grid[i][j]=='1'){
+                    cnt++;
+                    // cout<<i<<" "<<j<<endl;
+                    bfs(grid,vis,i,j);
+                }
+            }
+        }
+        return cnt;
     }
 };
