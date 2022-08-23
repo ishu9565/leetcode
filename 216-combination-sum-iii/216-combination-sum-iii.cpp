95b4419c442b06ca3a01a63eb88ddbcc880f5473
Ishu Gupta <60650077+ishu9565@users.noreply.github.com>
2022-08-23 16:23:46 +0530

    Time: 6 ms (21.96%), Space: 6.2 MB (94.93%) - LeetHub

---
+++ 216-combination-sum-iii/216-combination-sum-iii.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<vector<int>> combinationSum3(int k, int target) {
+        vector<int>candidates;
+        for(int i=1;i<=9;i++){
+            candidates.push_back(i);
+        }
+        std::sort(candidates.begin(), candidates.end());
+        std::vector<std::vector<int> > res;
+        std::vector<int> combination;
+        combinationSum2(candidates, target, res, combination, 0,k);
+        return res;
+    }
+    void combinationSum2(std::vector<int> &candidates, int target, std::vector<std::vector<int> > &res, std::vector<int> &combination, int begin,int k) {
+        if (!target) {
+            if(combination.size()==k)
+            res.push_back(combination);
+            return;
+        }
+        for (int i = begin; i != candidates.size() && target >= candidates[i]; ++i)
+            if (i == begin || candidates[i] != candidates[i - 1]) {
+                combination.push_back(candidates[i]);
+                combinationSum2(candidates, target - candidates[i], res, combination, i + 1,k);
+                combination.pop_back();
+            }
+    }
+};
