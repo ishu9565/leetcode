d90e63b686d5e739987ebf05dfebb12f278d0822
Ishu Gupta <60650077+ishu9565@users.noreply.github.com>
2022-08-26 16:52:39 +0530

    Time: 79 ms (31.89%), Space: 28.3 MB (8.94%) - LeetHub

--- 1010-pairs-of-songs-with-total-durations-divisible-by-60/1010-pairs-of-songs-with-total-durations-divisible-by-60.cpp
+++ 1010-pairs-of-songs-with-total-durations-divisible-by-60/1010-pairs-of-songs-with-total-durations-divisible-by-60.cpp
@@ -1,22 +1,17 @@
 class Solution {
 public:
     int numPairsDivisibleBy60(vector<int>& time) {
-        unordered_map<long long,long long> m;
-        long long ans=0;
+        unordered_map<int,int> m;
+        int ans=0;
         for(auto a: time){
-            m[a%60]++;
+           int t=a%60;
+            if(t==0)
+           ans+=m[0];
+           else
+           ans+=m[60-t];
+            m[t]++;
         }
-        for(int i=1;i<30;i++){
-            if(m.find(60-i)!=m.end()&&m.find(i)!=m.end()){
-                ans+=m[60-i]*m[i];
-            }
-        }
-        if(m.find(0)!=m.end()){
-            ans+=m[0]*(m[0]-1)/2;
-        }
-         if(m.find(30)!=m.end()){
-            ans+=m[30]*(m[30]-1)/2;
-        }
+        
         return ans;
     }
 };
