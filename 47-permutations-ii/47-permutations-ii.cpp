2f1078ed455400098a5b8d6bcab038165ce14159
Ishu Gupta <60650077+ishu9565@users.noreply.github.com>
2022-08-30 16:56:15 +0530

    Time: 14 ms (59.86%), Space: 9.1 MB (65.33%) - LeetHub

--- 47-permutations-ii/47-permutations-ii.cpp
+++ 47-permutations-ii/47-permutations-ii.cpp
@@ -1,17 +1,34 @@
 class Solution {
 public:
-    vector<vector<int>> permuteUnique(vector<int>& nums) {
-       // sort(nums.begin(),nums.end());
-        vector<vector<int>> ans;
-        
-        vector<int>temp;
-        temp=nums;
-        ans.push_back(nums);
-        next_permutation(nums.begin(),nums.end());
-        while(nums!=temp){
-            ans.push_back(nums);
-        next_permutation(nums.begin(),nums.end());
+    
+      void permut (vector<vector<int>> &res,int ind,vector<int> nums){
+        if(ind==nums.size()-1){
+            res.push_back(nums);
+            return ;
         }
-        return ans;
+        for(int i=ind;i<nums.size();i++){
+            if(ind!=i&&nums[ind]==nums[i]) continue;
+            swap(nums[i],nums[ind]);
+            permut(res,ind+1,nums);
+            // swap(nums[i],nums[ind]);
+        }
+    }
+    vector<vector<int>> permuteUnique(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+       vector<vector<int>> res;
+        permut(res,0,nums);
+        return res;
     }
 };
+
+// void recursion(vector<int> num, int i, int j, vector<vector<int> > &res) {
+//         if (i == j-1) {
+//             res.push_back(num);
+//             return;
+//         }
+//         for (int k = i; k < j; k++) {
+//             if (i != k && num[i] == num[k]) continue;
+//             swap(num[i], num[k]);
+//             recursion(num, i+1, j, res);
+//         }
+//     }
