8c43dd67a03e573e9185d49ffde1e0594e0c698c
Ishu Gupta <60650077+ishu9565@users.noreply.github.com>
2022-08-30 17:31:05 +0530

    Time: 31 ms (5.02%), Space: 57.9 MB (5.44%) - LeetHub

--- 377-combination-sum-iv/377-combination-sum-iv.cpp
+++ 377-combination-sum-iv/377-combination-sum-iv.cpp
@@ -1,16 +1,24 @@
 class Solution {
 public:
-    int combinationSum4(vector<int>& nums, int target) {
-         double  dp[target + 1];
-        for(int i=0;i<target+1;i++) dp[i]=0;
-        dp[0] = 1;
-        for (int i = 1; i <= target; i++) {
-            for (int n : nums) {
-                if (i >= n) {
-                    dp[i] +=  dp[i - n];
-                }
-            }
+
+    int count(vector<int> nums,int target, vector<int>&dp){
+        if(target<0) return 0;
+            if(target==0)  {
+                return 1;
+          }
+        if(dp[target]!=-1) return dp[target];
+        int ans=0;
+   for(int i:nums)
+        {
+           ans+= count(nums,target-i,dp);
         }
-        return dp[target];
+        return dp[target]=ans;;
+    }
+    
+    int combinationSum4(vector<int>& nums, int target) {
+        int ans=0;
+        vector<int>dp(1000,-1);
+        return  count(nums,target,dp);
+        // return ans;
     }
 };
