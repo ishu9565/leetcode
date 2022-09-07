4460257c850b93d5971aa9c7c05eec68345578b8
Ishu Gupta <60650077+ishu9565@users.noreply.github.com>
2022-09-07 16:33:37 +0530

    Time: 454 ms (48.69%), Space: 0B (100.00%) - LeetHub

--- 177-nth-highest-salary/177-nth-highest-salary.sql
+++ 177-nth-highest-salary/177-nth-highest-salary.sql
@@ -4,15 +4,10 @@ BEGIN
 DECLARE M INT;
 SET M=N-1;
   RETURN (
-     select
-      case 
-      when count(salary)< M then null
-      else(
+    
           select distinct salary from Employee
           order by salary desc limit 1 offset M
-      )
-      END
-      from Employee
+      
       # # Write your MySQL query statement below.
       # select (
       #     select distinct salary from Employee
